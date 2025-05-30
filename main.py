2430072a1c258cb024e39d0d74be3a8ecc7e0f2d
Paul Smith <psmith@magna.co.uk>
2025-05-30 09:59:52 +0100

    Added line drawing

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ class Window:
     def __init__(self, width, height):
         self.root = Tk()
         self.root.title("Maze Solver")
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
         self.canvas = Canvas(self.root, bg="white", height=height, width=width)
         self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
 
     def redraw(self):
         self.root.update_idletasks()
@@ -23,8 +26,27 @@ class Window:
         self.running = False
 
 
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+    
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2)
+
+
 def main():
     window = Window(800, 600)
+    line = Line(Point(100, 100), Point(700, 500))
+    window.draw_line(line, "black")
     window.wait_for_close()
 
 
